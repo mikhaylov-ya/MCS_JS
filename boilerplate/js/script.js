789300cb41dbbda3f498cbbf892652b180e7e9b3
StillConfused5 <sponaproz@gmail.com>
2021-09-11 18:14:44 +0300

    JS BP fix

--- boilerplate/js/script.js
+++ boilerplate/js/script.js
@@ -1,1 +1,10 @@
-console.log('You are at '+window.location);
+let totalCash = parseFloat(prompt('How much $$$?'));
+let watchesCount = parseInt(prompt('How many watches?'));
+let earringsCount = parseInt(prompt('How many earrings?'));
+
+let watchesSum = watchesCount * parseFloat(prompt('Watches price'));
+let earringsSum = earringsCount * parseFloat(prompt('Earrings price'));
+
+const isEnough = totalCash >= (watchesSum + earringsSum);
+
+document.body.innerHTML = isEnough;
