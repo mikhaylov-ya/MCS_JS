8bbed7dd401b9282ce54a945fd2e61c2aefcebcb
StillConfused5 <sponaproz@gmail.com>
2021-09-19 22:18:08 +0300

    p#5.9.1

--- playground/js/script.js
+++ playground/js/script.js
@@ -1,6 +1,10 @@
-const colored = document.querySelector('.colored');
-console.log(colored);
 
-let quest = prompt("Какой тебе цвет?");
+const page = document.querySelector('.page');
 
-colored.style['background-color'] = quest;
+function myFunc() {
+	for (let i = 1; i <= 100000; i++)
+		if (i % 6 === 0) page.innerText += ('\n' + i)
+};
+
+myFunc();
+
